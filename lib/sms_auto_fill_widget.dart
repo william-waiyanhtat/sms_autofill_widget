0d5fd0138b41be1d2a00191ba9ca48b4ea9f0d28
waiyan <waiyanhtat77@gmail.com>
2020-10-16 18:05:15 +0630

    pubspec yaml update

--- lib/sms_auto_fill_widget.dart
+++ lib/sms_auto_fill_widget.dart
@@ -1,7 +1,46 @@
 library sms_auto_fill_widget;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+import 'package:sms_autofill/sms_autofill.dart';
+
+// /// A Calculator.
+// class Calculator {
+//   /// Returns [value] plus 1.
+//   int addOne(int value) => value + 1;
+// }
+
+class SmsAutoFillWidget extends StatefulWidget {
+  @override
+  _SmsAutoFillWidgetState createState() => _SmsAutoFillWidgetState();
+}
+
+class _SmsAutoFillWidgetState extends State<SmsAutoFillWidget> {
+  String _code;
+  String signature = "{{ app signature }}";
+  int _codeLength = 4;
+
+  @override
+  void initState() {
+    super.initState();
+    _listenOTP();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+      child: PinFieldAutoFill(
+        codeLength: _codeLength,
+        onCodeChanged: (val) {},
+      ),
+    );
+  }
+}
+
+void _listenOTP() async {
+  await SmsAutoFill().listenForCode;
+}
+
+void getAppSignature() async {
+  await SmsAutoFill().getAppSignature;
 }
